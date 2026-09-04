1dcc942cbc309ed182774b361fb5453b23471a2a
Xpert-digital-max <chorawat1@gmail.com>
2026-09-05 02:56:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
 const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyzFxHS2LMwdh0d2BvggdBiUMDY79wRQr_xfJLBUzo8RB9XbxPQIWaNe6S8PpvGeAJDmw/exec";
+const MY_LIFF_ID = "2000218686-kY7ah48L"; // LIFF เช็คประวัติ (ของเดิมของคุณ)
+
+// 🌟 เพิ่มบรรทัดนี้เข้าไปสำหรับหน้าสมัครตัวแทน
+const AGENT_LIFF_ID = "2000218686-4vuDrxS0";
